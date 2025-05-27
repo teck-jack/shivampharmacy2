78be76efce11cff635a7ecbdcf776d442364049a
tack-jack <ds9321673@gmail.com>
2025-05-27 16:01:20 +0530

    add links agai after solve

--- src/data/productData.ts
+++ src/data/productData.ts
@@ -16,8 +16,8 @@ export const products: Product[] = [
     isNew: false,
     discount: 5,
     purchaseLinks: {
-      amazon: "https://www.amazon.com/Hajma-Churna/dp/...",
-      meesho: "https://www.meesho.com/Hajma-Churna/p..."
+      amazon: "https://www.amazon.in/churan-Acidity-Constipation-Digestion-Issues/dp/B0F7GJ6853/ref=sr_1_7?crid=139K6UUT8B684&dib=eyJ2IjoiMSJ9.P-iAVve0OhIzBlbMc5U11q-Ca4a2Ka5V6S8-4mZSCxQzvRJJYHRv0i59LcsuEA4sRlSVL7cAd3cTkfAo0_QKOsFQ51aOzUxsSwwm8I1JwtWKGW7U6ghJLIuBRphQGJ-rCG3N93OgZPlDf-BxLlWzLn1wIp_b-UrJI2hwvS6UcegVIf3qHDudcuRPdDBBWMQJ7tuOj9STuYsgmfBo4IaEoUwCvTC51IGQiEeupNh9GUnvOWP2oTjFaH2suDWQ4vRzcM0dk4SkjnKpWQNL-GrkdePMqtjjEZRyuzZjDfpn7sA.1ucV6R0kFBM3ims0Mj4KN0puVaO--_VJQNLHVLv0mww&dib_tag=se&keywords=hazma+powder&qid=1746458003&sprefix=hazma+powder%2Caps%2C329&sr=8-7",
+      meesho: "https://www.meesho.com/hajma-churan/p/8v0be5?ms=2&source=Meri+Shop"
     }
 
   },
@@ -35,8 +35,8 @@ export const products: Product[] = [
     isNew: false,
     discount: 7,
     purchaseLinks: {
-      amazon: "https://www.amazon.com/Hajma-Churna/dp/...",
-      meesho: "https://www.meesho.com/Hajma-Churna/p..."
+      amazon: "https://www.amazon.in/Compatible-Bahubali-Ayurvedic-Enhances-capacity/dp/B0F7GKR263/ref=sr_1_5?crid=KLCD6B5BBSQI&dib=eyJ2IjoiMSJ9.iL6G_3PDegafoeDuAxn8O_EEIEwH-iK8acFOenqaOGYWZvi0phBEN_9ogPe57Pj1vyQ208EkyW4HJh_Q0-2BLIUMmAl-mY-s64O_8HuT1rGt5RXy1KO4WPt-qRLdaNhqpsI38VveKBNTayjlQjoqzcEveavGD9Z6qNtXTMU67XIRx4jiWoXpfL__mUjHQgwXWdwzPyvBUdOi2SXaOzqjQTnQysr4g1XDGqit39XppZOQndPVgddiN1pNH2v6oBS4TBV7DAgFL9NdnHyot8cdPAtwKhwnxT1XFHAEeeFBmyI.Dqoc54_S8oOApAxxOaydQwpln9VO_epgs1NqjbZ66q8&dib_tag=se&keywords=bahubali+body+gainer+powder&qid=1746457914&sprefix=bahubali+body+gainer+piowder+%2Caps%2C362&sr=8-5",
+      meesho: "https://www.meesho.com/bahubali-body-gainer-ayurvedic-powder/p/8v0bw3?ms=2&source=Meri+Shop"
     }
   },
   {
@@ -52,8 +52,8 @@ export const products: Product[] = [
     isNew: true,
     discount: 5,
     purchaseLinks: {
-      amazon: "https://www.amazon.com/Hajma-Churna/dp/...",
-      meesho: "https://www.meesho.com/Hajma-Churna/p..."
+      amazon: "https://www.amazon.in/Ayurvedic-Advanced-Growth-Health-flavour/dp/B0F7GJHS3D/ref=sr_1_28?crid=C1ZWARDRDY3V&dib=eyJ2IjoiMSJ9._Saqjpw1ohos7McmSHWyeP_i3XJUPtrrZC-ED2efZZg6_8dFf2-pMo1UdjbRON0FwkcA6zmASRDZb3j4G7zQ29LeYRhxgFo7UXMhi_WplVGLtp4oqwQdIYcssRdDEehsYAEo6EjdOYtp767TOAqIVE16MrRe5sPo0SUbcUEMX425BPwO4-pZT5TZtEaVGNwUG44dVbLz835e73aHq7nALH1jhq9Ev_P5kftgv1IuDBYJTl9xF_zXrlXxuKLsOYrn5W92ftv3wtg35DDUVweVNAosJD71rpPVIB7KkY96G6g.QGp7P6Xhk-3C7Y9hRDMjxVW2f4_nwSJf21ZBaJxf7Ts&dib_tag=se&keywords=ayurvedic+advance+growth+powder&qid=1746458087&sprefix=aurvedic+advance+%2Caps%2C331&sr=8-28",
+      meesho: "https://www.meesho.com/ayurvedic-health-growth-powder/p/8v0bhi?ms=2&source=Meri+Shop"
     }
   },
   
@@ -70,8 +70,8 @@ export const products: Product[] = [
     isNew: false,
     discount: 5,
     purchaseLinks: {
-      amazon: "https://www.amazon.com/Hajma-Churna/dp/...",
-      meesho: "https://www.meesho.com/Hajma-Churna/p..."
+      amazon: "https://www.amazon.in/Ayurvedic-Advanced-Growth-Health-flavour/dp/B0F7GJHS3D/ref=sr_1_28?crid=C1ZWARDRDY3V&dib=eyJ2IjoiMSJ9._Saqjpw1ohos7McmSHWyeP_i3XJUPtrrZC-ED2efZZg6_8dFf2-pMo1UdjbRON0FwkcA6zmASRDZb3j4G7zQ29LeYRhxgFo7UXMhi_WplVGLtp4oqwQdIYcssRdDEehsYAEo6EjdOYtp767TOAqIVE16MrRe5sPo0SUbcUEMX425BPwO4-pZT5TZtEaVGNwUG44dVbLz835e73aHq7nALH1jhq9Ev_P5kftgv1IuDBYJTl9xF_zXrlXxuKLsOYrn5W92ftv3wtg35DDUVweVNAosJD71rpPVIB7KkY96G6g.QGp7P6Xhk-3C7Y9hRDMjxVW2f4_nwSJf21ZBaJxf7Ts&dib_tag=se&keywords=ayurvedic+advance+growth+powder&qid=1746458087&sprefix=aurvedic+advance+%2Caps%2C331&sr=8-28",
+      meesho: "https://www.meesho.com/advance-healthy-growth-powder/p/8v0bjn?ms=2&source=Meri+Shop"
     }
   },
   {
@@ -87,9 +87,10 @@ export const products: Product[] = [
     waight:"225 gm",
     isNew: false,
     discount: 5,
-    purchaseLinks: {
-      amazon: "https://www.amazon.com/Hajma-Churna/dp/...",
-      meesho: "https://www.meesho.com/Hajma-Churna/p..."
+    // dublicate 
+     purchaseLinks: {
+      amazon: "https://www.amazon.in/Compatible-Bahubali-Ayurvedic-Enhances-capacity/dp/B0F7GKR263/ref=sr_1_5?crid=KLCD6B5BBSQI&dib=eyJ2IjoiMSJ9.iL6G_3PDegafoeDuAxn8O_EEIEwH-iK8acFOenqaOGYWZvi0phBEN_9ogPe57Pj1vyQ208EkyW4HJh_Q0-2BLIUMmAl-mY-s64O_8HuT1rGt5RXy1KO4WPt-qRLdaNhqpsI38VveKBNTayjlQjoqzcEveavGD9Z6qNtXTMU67XIRx4jiWoXpfL__mUjHQgwXWdwzPyvBUdOi2SXaOzqjQTnQysr4g1XDGqit39XppZOQndPVgddiN1pNH2v6oBS4TBV7DAgFL9NdnHyot8cdPAtwKhwnxT1XFHAEeeFBmyI.Dqoc54_S8oOApAxxOaydQwpln9VO_epgs1NqjbZ66q8&dib_tag=se&keywords=bahubali+body+gainer+powder&qid=1746457914&sprefix=bahubali+body+gainer+piowder+%2Caps%2C362&sr=8-5",
+      meesho: "https://www.meesho.com/bahubali-body-gainer-ayurvedic-powder/p/8v0bw3?ms=2&source=Meri+Shop"
     }
   },
   {
@@ -105,8 +106,8 @@ export const products: Product[] = [
     isNew: true,
     discount: 5,
     purchaseLinks: {
-      amazon: "https://www.amazon.com/Hajma-Churna/dp/...",
-      meesho: "https://www.meesho.com/Hajma-Churna/p..."
+      amazon: "https://www.amazon.in/Compatible-Bahubali-Ayurvedic-Enhances-capacity/dp/B0F7GKR263/ref=sr_1_5?crid=KLCD6B5BBSQI&dib=eyJ2IjoiMSJ9.iL6G_3PDegafoeDuAxn8O_EEIEwH-iK8acFOenqaOGYWZvi0phBEN_9ogPe57Pj1vyQ208EkyW4HJh_Q0-2BLIUMmAl-mY-s64O_8HuT1rGt5RXy1KO4WPt-qRLdaNhqpsI38VveKBNTayjlQjoqzcEveavGD9Z6qNtXTMU67XIRx4jiWoXpfL__mUjHQgwXWdwzPyvBUdOi2SXaOzqjQTnQysr4g1XDGqit39XppZOQndPVgddiN1pNH2v6oBS4TBV7DAgFL9NdnHyot8cdPAtwKhwnxT1XFHAEeeFBmyI.Dqoc54_S8oOApAxxOaydQwpln9VO_epgs1NqjbZ66q8&dib_tag=se&keywords=bahubali+body+gainer+powder&qid=1746457914&sprefix=bahubali+body+gainer+piowder+%2Caps%2C362&sr=8-5",
+      meesho: "https://www.meesho.com/bahubali-body-gainer-ayurvedic-powder/p/8v0bw3?ms=2&source=Meri+Shop"
     }
   },
 ]
